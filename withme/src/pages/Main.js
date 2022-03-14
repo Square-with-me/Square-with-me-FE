4ad66c8e733a5bb5a1eb929e674ca6317271069c
yun2021 <77830226+yun2021@users.noreply.github.com>
2022-03-14 13:30:03 +0900

    메인 버튼 및 카테고리 CSS 완, 아이콘 바꾸기 완

--- withme/src/pages/Main.js
+++ withme/src/pages/Main.js
@@ -9,7 +9,6 @@ import { ReactComponent as Search } from '../assets/main/searchIcon.svg';
 import { ReactComponent as Plus } from '../assets/main/plusIcon.svg';
 import { ReactComponent as Lock } from '../assets/main/lockIcon.svg';
 // import { ReactComponent as HotRoom } from '../assets/main/hotRoomIcon.svg';
-// import { ReactComponent as LowerAngle } from '../assets/main/lowerAngleIcon.svg';
 import { ReactComponent as Refresh } from '../assets/main/refreshIcon.svg';
 import { ReactComponent as UserNick } from '../assets/main/userNickIcon.svg';
 
@@ -184,51 +183,91 @@ const Main = () => {
             </SpectatorBtn>
           </div>
 
-          <div className="container">
-            <div className="menu-container">
-              <DropBtn
-                onClick={() => setIsActive(!isActive)}
-                className="menu-trigger"
-              >
-                <CategoryDText>{category}</CategoryDText>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M20.9001 6.54999L12.0001 15.45L3.1001 6.54999" stroke="#8A8BA3" stroke-width="2" stroke-miterlimit="10" />
-                </svg>
-              </DropBtn>
+          <Category>
+            <div className="container">
+              <DropWrap>
+                <DropBtn
+                  onClick={() => setIsActive(!isActive)}
+                  style={{ height: "40px" }}
+                >{category === ""
+                ? <CategoryDText>카테고리</CategoryDText>
+                :<CategoryDText>{category}</CategoryDText>}
+                  <div>
+                    <svg width="24" height="24" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M24.9001 10.5498L16.0001 19.4498L7.1001 10.5498" stroke="#8A8BA3" stroke-width="2" stroke-miterlimit="10"/>
+                    </svg>
+                  </div>
+                </DropBtn>
+                <div                  
+                  ref={dropdownRef}
+                  className={`menu ${isActive ? "active" : "inactive"}`}>
+                  <CategoryWrap
+                  onClick={()=>{
+                    setCategory("뷰티")
+                    setIsActive(!isActive)
+                    setChoiceCate(1);
+                  }}>
+                    <div>뷰티</div>
+                    <svg width="24px" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M13.11 9.32L11.33 5H8.88L7.1 9.32L4 11.44V14.18L7.1 16.3L9 21H11.16L13.08 16.32L15.97 14.32V11.32L13.11 9.32ZM11.49 15L10.11 18.36L8.72 15L5.54 12.81L8.72 10.64L10.11 7.27L11.49 10.64L14.67 12.81L11.49 15Z" fill="#8A8BA3"/>
+                      <path d="M23.7799 15.34L21.9999 11H19.5499L17.7699 15.32L14.6699 17.44V20.2L17.7699 22.32L19.6999 27H21.8599L23.7799 22.32L26.6699 20.32V17.32L23.7799 15.34ZM22.1599 21L20.7799 24.36L19.3899 21L16.2099 18.82L19.3899 16.65L20.7799 13.28L22.1599 16.65L25.3399 18.82L22.1599 21Z" fill="#8A8BA3"/>
+                    </svg>
+                  </CategoryWrap>
 
-              <div                  
-                ref={dropdownRef}
-                className={`menu ${isActive ? "active" : "inactive"}`}>
-                <CategoryWrap>
-                  <div>뷰티</div>
-                  <svg width="24px" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M13.11 9.32L11.33 5H8.88L7.1 9.32L4 11.44V14.18L7.1 16.3L9 21H11.16L13.08 16.32L15.97 14.32V11.32L13.11 9.32ZM11.49 15L10.11 18.36L8.72 15L5.54 12.81L8.72 10.64L10.11 7.27L11.49 10.64L14.67 12.81L11.49 15Z" fill="#8A8BA3"/>
-                    <path d="M23.7799 15.34L21.9999 11H19.5499L17.7699 15.32L14.6699 17.44V20.2L17.7699 22.32L19.6999 27H21.8599L23.7799 22.32L26.6699 20.32V17.32L23.7799 15.34ZM22.1599 21L20.7799 24.36L19.3899 21L16.2099 18.82L19.3899 16.65L20.7799 13.28L22.1599 16.65L25.3399 18.82L22.1599 21Z" fill="#8A8BA3"/>
-                  </svg>
-                </CategoryWrap>
-                <CategoryWrap>
-                  <div>운동</div>
-                  <Exercise fill="#8A8BA3" width="24px"/>
-                </CategoryWrap>
-                <CategoryWrap>
-                  <div>스터디</div>
-                  <Study fill="#8A8BA3" width="24px"/>
-                </CategoryWrap>
-                <CategoryWrap>
-                  <div>상담</div>
-                  <Consulting fill="#8A8BA3" width="24px"/>
-                </CategoryWrap>
-                <CategoryWrap>
-                  <div>문화</div>
-                  <div><Culture fill="#8A8BA3" width="24px"/></div>
-                </CategoryWrap>
-                <CategoryWrap>
-                  <div>기타</div>
-                  <Other fill="#8A8BA3" width="24px"/>
-                </CategoryWrap>
-              </div>
+                  <CategoryWrap                   
+                  onClick={()=>{
+                    setCategory("운동")
+                    setIsActive(!isActive)
+                    setChoiceCate(2);
+                  }}>
+                    <div>운동</div>
+                    <Exercise fill="#8A8BA3" width="24px"/>
+                  </CategoryWrap>
+
+                  <CategoryWrap                   
+                  onClick={()=>{
+                    setCategory("스터디")
+                    setIsActive(!isActive)
+                    setChoiceCate(3);
+                  }}>
+                    <div>스터디</div>
+                    <Study fill="#8A8BA3" width="24px"/>
+                  </CategoryWrap>
+
+                  <CategoryWrap                   
+                  onClick={()=>{
+                    setCategory("상담")
+                    setIsActive(!isActive)
+                    setChoiceCate(4);
+                  }}>
+                    <div>상담</div>
+                    <Consulting fill="#8A8BA3" width="24px"/>
+                  </CategoryWrap>
+
+                  <CategoryWrap                   
+                  onClick={()=>{
+                    setCategory("문화")
+                    setIsActive(!isActive)
+                    setChoiceCate(5);
+                  }}>
+                    <div>문화</div>
+                    <div><Culture  fill="#8A8BA3" width="24px"/></div>
+                  </CategoryWrap>
+
+                  <CategoryWrap                   
+                  onClick={()=>{
+                    setCategory("기타")
+                    setIsActive(!isActive)
+                    setChoiceCate(6);
+                  }}>
+                    <div>기타</div>
+                    <Other fill="#8A8BA3" width="24px"/>
+                  </CategoryWrap>
+                </div>
+              </DropWrap>
             </div>
-          </div>
+
+          </Category>
           <Refresh
             style={{
               cursor: "pointer",
@@ -468,39 +507,43 @@ const EXRoomMaker = styled.button`
 `;
 
 const AllBtn = styled.button`
-  padding: 17px 14px;
+  padding: 14px 0px;
   width: 56px;
-  height: 48px;
+  height: 43px;
   border: 1px solid #8a8ba3;
   border-radius: 4px;
   margin-right: 16px;
   background-color: #ffffff;
+  font-weight: 500;
+  font-size: 16px;
   :hover {
     box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
   }
 `;
 
 const PossibleBtn = styled.button`
-  padding: 12px 14px;
   width: 90px;
-  height: 48px;
+  height: 43px;
   border: 1px solid #8a8ba3;
   border-radius: 4px;
   margin-right: 16px;
   background-color: #ffffff;
+  font-weight: 500;
+  font-size: 16px;
   :hover {
     box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
   }
 `;
 
 const SpectatorBtn = styled.button`
-  padding: 12px 14px;
   width: 90px;
-  height: 48px;
+  height: 43px;
   border: 1px solid #8a8ba3;
   border-radius: 4px;
   margin-right: 16px;
   background-color: #ffffff;
+  font-weight: 500;
+  font-size: 16px;
   :hover {
     box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
   }
@@ -523,16 +566,30 @@ const Btn = styled.button`
   }
 `;
 
-const CategoryDText = styled.div`
-  size: 2rem;
-  color: #8a8ba3;
-`;
-
 const MenuBar = styled.div`
   display: flex;
   margin-bottom: 25px;
 `;
 
+const Category = styled.div`
+  display: flex;
+  margin: 1px 10px 16px 0px;
+  select{
+    width: 148px;
+    height: 40px;
+    border: 1px solid #8A8BA3;
+    border-radius: 4px;
+    & :focus{
+      border: 1px solid #7179F0 ;
+    }
+  }
+  select > option {
+    width: 100%;
+    background-color: blue;
+  }
+`;
+
+//드롭부분 css
 const DropBtn = styled.button`
   display: flex;
   justify-content: space-between;
@@ -543,7 +600,43 @@ const DropBtn = styled.button`
   padding: 15px;
   border: none;
   width: 200px;
+  &:hover{
+    box-shadow: 0 1px 5px rgba(0, 0, 0, 0.3);
+    border: 1px solid #7B61FF
+  }
+  .active{
+    border: 1px solid #7B61FF;
+  }
 `;
+
+const CategoryDText = styled.div`
+  font-size: 16px;
+  color: #8a8ba3;
+`;
+
+const DropWrap =styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #8A8BA3;
+  border-radius: 4px;
+  width: 140px;
+`
+const CategoryWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+  align-items: center;
+  div{
+    font-size: 16px;
+    color: #8a8ba3;
+    &:hover{
+      color: #7179F0 ;
+      fill: #7179F0;
+    }
+  }
+`
 
 //searchbar
 const SearchBarWrap = styled.div`
@@ -622,21 +715,6 @@ const CategoryText = styled.div`
   display: flex;
   justify-content: center;
 `;
-
-const CategoryWrap = styled.div`
-display: flex;
-justify-content: space-between;
-padding: 10px;
-align-items: center;
-div{
-  font-size: 16px;
-  color: #8a8ba3;
-  &:hover{
-    color: #7179F0 ;
-    fill: #7179F0;
-  }
-}
-`
 
 const TitleText = styled.div`
   font-weight: 700;
