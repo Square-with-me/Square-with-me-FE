25d3b2ab10c51a948c7f7d1a5fa3ccd05ebc0fda
Sumin Park <clappingmin@gmail.com>
2022-03-17 17:01:37 +0900

    랜딩페이지 제작중

--- withme/src/pages/Landing.js
+++ withme/src/pages/Landing.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
+import $ from 'jquery';
+import smileIcon from '../assets/emoticon/smileIcon.svg';
+import Logo from '../components/Main/Logo';
 
 const Landing = () => {
   return (
     <Container>
-      {/* header */}
       <header>
         <div className="header-inner">
-          <div className="logo">
-            <a href="#none">
-              <image src=""></image>
-            </a>
+          <div className="gnb">
+            <a href="#none">서비스 설명</a>
+            <a href="#none">튜토리얼</a>
+            <a href="#none">사용후기</a>
+            <a href="#none">시작하기</a>
           </div>
-          <div className="gnb"></div>
         </div>
       </header>
-      <section className="welcome"></section>
+      <section className="welcome">
+        <Logo></Logo>
+      </section>
       <section className="ceo-access"></section>
       <section className="banner"></section>
       <section className="feature"></section>
@@ -32,14 +36,29 @@ const Landing = () => {
 };
 
 const Container = styled.div`
-  max-width: 1110px;
-  height: 100vh;
-  margin: auto;
-
-  background-color: skyblue;
+  background-color: #fbfbfb;
   header {
+    width: 100%;
+    background-color: #c4c4c4;
+    .header-inner {
+      width: 1100px;
+      margin: auto;
+      overflow: hidden;
+      height: 80px;
+      display: flex;
+      justify-content: right;
+      align-items: center;
+      .gnb {
+        a {
+          margin: 30px; // a 태그는 inine요소라서 위아래는 margin이 안걸린다.
+          font-size: 25px;
+          font-weight: 700;
+          color: #000;
+          text-decoration: none;
+        }
+      }
+    }
   }
-
   .welcome {
   }
   .ceo-access {
