9b090b6ea356332780c0403f8293e17fefee908d
Sumin Park <clappingmin@gmail.com>
2022-03-19 01:40:52 +0900

    오늘 데이터 없을때 그래프 추가

--- withme/src/components/Mypage/TodayTime.js
+++ withme/src/components/Mypage/TodayTime.js
@@ -4,13 +4,13 @@ import { Pie } from 'react-chartjs-2';
 import { useEffect, useState } from 'react';
 
 const TodayTime = () => {
-  const [dayList, setDayList] = useState([0, 0, 0, 0, 0, 0]);
+  const [dayList, setDayList] = useState(null);
 
   useEffect(() => {
     const day = JSON.parse(localStorage.getItem('time'));
     if (day) setDayList([day[1], day[2], day[3], day[4], day[5], day[6]]);
-    else return;
-  }, [setDayList]);
+  }, [dayList]);
+
   const data = {
     labels: ['뷰티', '운동', '스터디', '문화', '상담', '기타'],
     datasets: [
@@ -28,7 +28,6 @@ const TodayTime = () => {
       },
     ],
   };
-
   const option = {
     plugins: {
       legend: {
@@ -45,7 +44,34 @@ const TodayTime = () => {
     },
   };
 
-  return <Pie data={data} options={option} />;
+  const emptyData = {
+    labels: ['오늘은 기록이 아직 없어요.'],
+    datasets: [
+      {
+        data: [100],
+        backgroundColor: ['#808080'],
+        hoverOffset: 4,
+      },
+    ],
+  };
+  const emptyOption = {
+    plugins: {
+      legend: {
+        display: false,
+      },
+      tooltip: {
+        callbacks: {
+          label: function (context) {
+            let label = context.label;
+            return label;
+          },
+        },
+      },
+    },
+  };
+
+  if (dayList === null) return <Pie data={emptyData} options={emptyOption} />;
+  else return <Pie data={data} options={option} />;
 };
 
 export default TodayTime;
