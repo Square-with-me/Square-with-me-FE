976698b52a514316c5cb3b0e879286b4c7792003
Sumin Park <clappingmin@gmail.com>
2022-03-27 02:23:29 +0900

    마이페이지 수정

--- withme/src/components/Header/Kakao.js
+++ withme/src/components/Header/Kakao.js
@@ -1,20 +1,23 @@
-import React, { useEffect,} from "react";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { actionCreators as userActions } from "../../redux/modules/user";
-import Spinner from "react-bootstrap/Spinner"
-import styled from "styled-components";
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { actionCreators as userActions } from '../../redux/modules/user';
+// import Spinner from "react-bootstrap/Spinner"
+import styled from 'styled-components';
 
 const Kakao = () => {
   const dispatch = useDispatch();
-  let authorization_code = new URL(window.location.href).searchParams.get("code");
-  console.log("리디렉트해서 받은 코드", authorization_code);
+  let authorization_code = new URL(window.location.href).searchParams.get(
+    'code'
+  );
+  console.log('리디렉트해서 받은 코드', authorization_code);
 
   const kakaoLogin = async (authorization_code) => {
-    await axios.get(`/api/auth/kakao/callback?code=${authorization_code}`)
+    await axios
+      .get(`/api/auth/kakao/callback?code=${authorization_code}`)
       .then((res) => {
         localStorage.setItem('login-token', res.data.data.token);
-        const user = res.data.data.user
+        const user = res.data.data.user;
         dispatch(userActions.setUser({ user }));
         window.location.reload('/main');
         // const token = response.data.data.token;
@@ -23,7 +26,7 @@ const Kakao = () => {
         // console.log('유저', response.data);
       })
       .catch((error) => {
-        console.log("카카오 로그인실패", error);
+        console.log('카카오 로그인실패', error);
       });
   };
 
@@ -33,10 +36,10 @@ const Kakao = () => {
 
   return (
     <div>
-      <Spinner animation="border" role="status">
+      {/* <Spinner animation="border" role="status">
         <span className="visually-hidden">Loading...</span>
-      </Spinner>
+      </Spinner> */}
     </div>
   );
-}
+};
 export default Kakao;
--- withme/src/index.js
+++ withme/src/index.js
@@ -5,9 +5,9 @@ import App from './shared/App';
 import store from './redux/configureStore';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './shared/serviceWorker';
-import axios from 'axios'
-import {BackUrl} from "./shared/config"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { BackUrl } from './shared/config';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 // axios.defaults.withCredentials=true;
 axios.defaults.baseURL = `${BackUrl}`;
 
