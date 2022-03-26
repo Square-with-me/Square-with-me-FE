b65d0a0fa1ed36c18990796a65b8566451d05af8
zinny22 <hyejinsang@naver.com>
2022-03-26 15:51:26 +0900

    이벤트 링크 수정

--- withme/src/components/Main/Banner.js
+++ withme/src/components/Main/Banner.js
@@ -5,12 +5,15 @@ import Event from "../Modal/Event";
 
 const Banner =()=>{
   const [event, setEvent] = useState(false)
+  const CoffeeEvent =()=>{
+    window.open("https://docs.google.com/forms/d/e/1FAIpQLSfjH1T4DhQp6fqdaB9Ordtpe3NMKw0bZAAl4IHTfc6e2ItN8w/viewform?usp=sf_link")  
+  }
  return (
    <React.Fragment>
     <div className="slide">
     <ul>
       <li onClick={()=>{setEvent(true)}}></li>
-      <li></li>
+      <li onClick={CoffeeEvent}></li>
       <li></li>
       <li></li>
     </ul>
