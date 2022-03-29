8444bdd472d9b4bb7001c18ef7a6c4443798ccd9
zinny22 <hyejinsang@naver.com>
2022-03-29 17:13:17 +0900

    오늘의 시간 수정

--- withme/src/shared/config.js
+++ withme/src/shared/config.js
@@ -1,2 +1,2 @@
-// export const BackUrl = "https://api.nemowithme.com"
-export const BackUrl = "http://14.45.204.153:7034"
+export const BackUrl = "https://api.nemowithme.com"
+// export const BackUrl = "http://14.45.204.153:7034"
